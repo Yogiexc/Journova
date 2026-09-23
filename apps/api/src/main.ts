fbe43d55a2bbfb1434ebc14aac63a38b0f6e40bf
Bryan Yogie Saputra <yogiexsaputra@gmail.com>
2026-09-23 11:29:08 +0700

    feat: implement core authentication and article management features across web and api apps

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor());
   
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
     credentials: true,
   });
 
